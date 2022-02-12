8926f00a07a0231465f39029d3a1427113b19625
snakajim <satoshi.nakajima@arm.com>
2022-02-12 22:14:59 +0900

    finalize

--- install_gcc_cmake_git_as_default.sh
+++ install_gcc_cmake_git_as_default.sh
@@ -6,7 +6,9 @@
 #   git: 2.33.1-2021-10-13 https://github.com/git/git.git -b v2.33.1
 # cmake: 3.18.6-2021-02-11 https://cmake.org/files/v3.18/cmake-3.18.6.tar.gz
 #
-# 
+# How to download:
+# $> curl https://raw.githubusercontent.com/snakajim/centos7_install_tools/main/install_gcc_cmake_git_as_default.sh > install_gcc_cmake_git_as_default.sh
+#
 GCC_VERSION=$(gcc --version | awk 'NR<2 { print $3 }' | awk -F. '{printf "%2d%02d%02d", $1,$2,$3}')
 CMAKE_VERSION=$(cmake --version | awk 'NR<2 { print $3 }' | awk -F. '{printf "%2d%02d%02d", $1,$2,$3}')
 GIT_VERSION=$(git --version | awk 'NR<2 { print $3 }' | awk -F. '{printf "%2d%02d%02d", $1,$2,$3}')
@@ -29,10 +31,13 @@ else
   mkdir -p ${HOME}/tmp/gcc && rm -rf ${HOME}/tmp/gcc/* && cd ${HOME}/tmp/gcc && \
     aria2c -x4 http://ftp.tsukuba.wide.ad.jp/software/gcc/releases/gcc-7.5.0/gcc-7.5.0.tar.gz
   cd ${HOME}/tmp/gcc && tar -zxvf gcc-7.5.0.tar.gz && cd gcc-7.5.0 && ./contrib/download_prerequisites
-  cd ${HOME}/tmp/gcc/gcc-7.5.0 && mkdir -p build && cd build && ../configure --enable-languages=c,c++ --prefix ${HOME}/local --disable-multilib
+  cd ${HOME}/tmp/gcc/gcc-7.5.0 && mkdir -p build && cd build && ../configure --enable-languages=c,c++ --prefix=/usr --disable-multilib
   cd ${HOME}/tmp/gcc/gcc-7.5.0/build && make -j`nproc`
-  sudo make install
-
+  cd ${HOME}/tmp/gcc/gcc-7.5.0/build && sudo make install
+  sudo mv /usr/lib64/libstdc++.so.6.0.24-gdb.py /usr/lib64/back_libstdc++.so.6.0.24-gdb.py
+  sudo sed -i -e '$ a /usr/lib64' /etc/ld.so.conf
+  sudo ldconfig -v
+  cd ${HOME}/tmp && rm -rf ${HOME}/tmp/gcc
 fi
 
 if [ $GIT_VERSION -gt 23000 ]; then
@@ -45,6 +50,7 @@ else
   cd ${HOME}/tmp/git && tar -zxvf git-2.33.1.tar.gz
   cd ${HOME}/tmp/git/git-2.33.1 && mkdir -p build && cd build && ../configure --prefix=/usr && make -j`nproc`
   cd ${HOME}/tmp/git/git-2.33.1/build && sudo make install
+  cd ${HOME}/tmp && rm -rf ${HOME}/tmp/git
 fi
 
 if [ $CMAKE_VERSION -gt 31500 ]; then
@@ -57,5 +63,6 @@ else
   cd ${HOME}/tmp/cmake && tar -zxvf cmake-3.18.6.tar.gz
   cd ${HOME}/tmp/cmake/cmake-3.18.6 && mkdir -p build && cd build && ../configure --prefix=/usr && make -j`nproc`
   cd ${HOME}/tmp/cmake/cmake-3.18.6/build && sudo make install
+  cd ${HOME}/tmp && rm -rf ${HOME}/tmp/cmake
 fi
 
