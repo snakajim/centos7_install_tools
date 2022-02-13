887014722f873f51045e65fc0a0c9fb35b33bbe2
snakajim <satoshi.nakajima@arm.com>
2022-02-13 15:27:29 +0000

    bug fix

--- install_verilator.sh
+++ install_verilator.sh
@@ -95,9 +95,9 @@ start_time=`date +%s`
 cd ${HOME}/tmp/verilator && autoconf && \
   ./configure --prefix=/usr/local/verilator_4_${VERILATOR_REV} \
   CC=$CC \
-  CXX=$CXX && \
-  make -j`nproc` &> ${HOME}/run_verilator${VERILATOR_REV}.log && \
-  sudo make install
+  CXX=$CXX
+make -j`nproc`
+sudo make install
 end_time=`date +%s`
 run_time=$((end_time - start_time))
 sudo ln -sf /usr/local/verilator_4_${VERILATOR_REV}/bin/verilator* /usr/local/verilator_4_${VERILATOR_REV}/share/verilator/bin/
