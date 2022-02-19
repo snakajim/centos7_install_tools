06065a909c2ada0251703256231965f05bf52404
snakajim <satoshi.nakajima@arm.com>
2022-02-19 06:07:06 +0000

    update

--- install_gcc_cmake_git_as_default.sh
+++ install_gcc_cmake_git_as_default.sh
@@ -14,9 +14,10 @@ CMAKE_VERSION=$(cmake --version | awk 'NR<2 { print $3 }' | awk -F. '{printf "%2
 GIT_VERSION=$(git --version | awk 'NR<2 { print $3 }' | awk -F. '{printf "%2d%02d%02d", $1,$2,$3}')
 CENTOS_VERSION=$(cat /etc/os-release | grep "PRETTY_NAME=" | sed -r 's#^PRETTY_NAME="CentOS\s+Linux\s+([0-9]).+#\1#')
 GMAKE_VERSION=$(make --version | awk 'NR<2 { print $3 }' | awk -F. '{printf "%2d%02d%02d", $1,$2,$3}')
+LD_VERSION=$(ld --version | awk 'NR<2 { print $7 }' | awk -F. '{printf "%2d%02d%02d", $1,$2,$3}')
 
 # install dependencies for build tools
-sudo yum -y install wget aria2 git
+sudo yum -y install wget aria2 git texinfo
 
 # OS Version check
 if [ $CENTOS_VERSION != "7" ]; then
@@ -39,6 +40,15 @@ else
   sudo sed -i -e '$ a /usr/lib64' /etc/ld.so.conf
   sudo ldconfig
   cd ${HOME}/tmp && rm -rf ${HOME}/tmp/gcc
+  echo "Also update gdb, replace under /usr/bin."
+  mkdir -p ${HOME}/tmp/gdb && rm -rf ${HOME}/tmp/gdb/* && cd ${HOME}/tmp/gdb && \
+    aria2c -x4 https://ftp.gnu.org/gnu/gdb/gdb-9.2.tar.gz
+  cd ${HOME}/tmp/gdb && tar -zxvf gdb-9.2.tar.gz && cd gdb-9.2
+  cd ${HOME}/tmp/gdb/gdb-9.2 && mkdir -p build && cd build && ../configure --prefix=/usr
+  cd ${HOME}/tmp/gdb/gdb-9.2/build && make -j`nproc`
+  cd ${HOME}/tmp/gdb/gdb-9.2/build && sudo make install
+  sudo ldconfig
+  cd ${HOME}/tmp && rm -rf ${HOME}/tmp/gdb
 fi
 
 if [ $GIT_VERSION -gt 23000 ]; then
@@ -81,4 +91,18 @@ else
   cd ${HOME}/tmp/gmake/make-4.2/build && sudo make install
   sudo ldconfig
   cd ${HOME}/tmp && rm -rf ${HOME}/tmp/gmake
+fi
+
+if [ $LD_VERSION -gt 23200 ]; then
+  echo "Your ld is new, no need to refresh. Installation skip."
+else
+  echo "Your ls is old, replace under /usr/bin."
+  #sudo yum -y remove cmake
+  mkdir -p ${HOME}/tmp/binutils && rm -rf ${HOME}/tmp/binutils/* && cd ${HOME}/tmp/binutils && \
+  aria2c -x4 https://ftp.gnu.org/gnu/binutils/binutils-2.34.tar.gz
+  cd ${HOME}/tmp/binutils && tar -zxvf binutils-2.34.tar.gz
+  cd ${HOME}/tmp/binutils/binutils-2.34 && mkdir -p build && cd build && ../configure --prefix=/usr && make -j`nproc`
+  cd ${HOME}/tmp/binutils/binutils-2.34/build && sudo make install
+  sudo ldconfig
+  cd ${HOME}/tmp && rm -rf ${HOME}/tmp/binutils
 fi
