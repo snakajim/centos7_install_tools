5668d705210ec2dfd9c56e80217c523dc8cef09c
snakajim <satoshi.nakajima@arm.com>
2022-10-10 21:55:46 +0900

    bug fix

--- install_armdev.sh
+++ install_armdev.sh
@@ -45,24 +45,13 @@ fi
 # Install and set path
 #
 sudo mkdir -p ${GCC_DIR}
-sudo tar -Jxvf arm-gnu-toolchain-${GCC_VER}-${HOSTARCH}-arm-none-eabi.tar.xz -C ${GCC_DIR}
-echo "# AArch32 bare-metal target (arm-none-eabi)"
-echo "export PATH=${GCC_DIR}/arm-gnu-toolchain-${GCC_VER}-${HOSTARCH}-arm-none-eabi/bin:\$PATH"
-sudo tar -Jxvf arm-gnu-toolchain-${GCC_VER}-${HOSTARCH}-arm-none-linux-gnueabihf.tar.xz -C ${GCC_DIR}
-echo "# AArch32 GNU/Linux target with hard float (arm-none-linux-gnueabihf)"
-echo "export PATH=${GCC_DIR}/arm-gnu-toolchain-${GCC_VER}-${HOSTARCH}-arm-none-linux-gnueabihf/bin:\$PATH"
-sudo tar -Jxvf arm-gnu-toolchain-${GCC_VER}-${HOSTARCH}-aarch64-none-elf.tar.xz -C ${GCC_DIR}
-echo "# AArch64 bare-metal target (aarch64-none-elf)"
-echo "export PATH=${GCC_DIR}/arm-gnu-toolchain-${GCC_VER}-${HOSTARCH}-aarch64-none-elf/bin:\$PATH" 
-
-#
-# Install glibc-2.34 https://ftp.gnu.org/gnu/glibc/glibc-2.34.tar.gz
-#
-mkdir -p ~/tmp/glibc && cd  ~/tmp/glibc && \
-  aria2c -x10 https://ftp.gnu.org/gnu/glibc/glibc-2.34.tar.gz && \
-  tar -zxvf glibc-2.34.tar.gz
-mkdir -p ~/tmp/glibc/glibc-2.34/build && cd ~/tmp/glibc/glibc-2.34/build && \
-  ../configure --prefix "/usr" && \
-  make -j `nproc` && \
-  sudo make install -j `nproc`
+sudo tar -Jxvf ~/tmp/${HOSTARCH}/arm-gnu-toolchain-${GCC_VER}-${HOSTARCH}-arm-none-eabi.tar.xz -C ${GCC_DIR}
+sudo tar -Jxvf ~/tmp/${HOSTARCH}/arm-gnu-toolchain-${GCC_VER}-${HOSTARCH}-arm-none-linux-gnueabihf.tar.xz -C ${GCC_DIR}
+sudo tar -Jxvf ~/tmp/${HOSTARCH}/arm-gnu-toolchain-${GCC_VER}-${HOSTARCH}-aarch64-none-elf.tar.xz -C ${GCC_DIR}
+echo "# AArch32 bare-metal target (arm-none-eabi)" >> ${HOME}/.bashrc
+echo "export PATH=${GCC_DIR}/arm-gnu-toolchain-${GCC_VER}-${HOSTARCH}-arm-none-eabi/bin:\$PATH" >> ${HOME}/.bashrc
+echo "# AArch32 GNU/Linux target with hard float (arm-none-linux-gnueabihf)" >> ${HOME}/.bashrc
+echo "export PATH=${GCC_DIR}/arm-gnu-toolchain-${GCC_VER}-${HOSTARCH}-arm-none-linux-gnueabihf/bin:\$PATH" >> ${HOME}/.bashrc
+echo "# AArch64 bare-metal target (aarch64-none-elf)" >> ${HOME}/.bashrc
+echo "export PATH=${GCC_DIR}/arm-gnu-toolchain-${GCC_VER}-${HOSTARCH}-aarch64-none-elf/bin:\$PATH" >> ${HOME}/.bashrc
   
